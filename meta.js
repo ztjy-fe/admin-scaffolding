798ed5d35dd3ae1b9aa5d509d2388296505201bf
ztjy-fe <zhangzhenglong@szy.cn>
2018-03-14 10:55:40 +0800

    新增是否使用tabs形式切换路由

--- meta.js
+++ meta.js
@@ -39,8 +39,8 @@ module.exports = {
 		}
 	},
 	"filters": {
-		"src/assets/images/lazyload/*": "VueLazyload"
-		"src/store/modules/tabs.js": "tabs"
+		"src/assets/images/lazyload/*": "VueLazyload",
+		"src/store/modules/tabs.js": "tabs",
 		"src/view/layout/components/Tabs.vue": "tabs"
 	},
 	"completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/ztjy-fe/scaffolding"
