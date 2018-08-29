d1c5652cd21a4e6a87b7844061a020c6548c216e
ztjy-fe <zhangzhenglong@szy.cn>
2018-08-29 11:08:04 +0800

    update

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -22,94 +22,94 @@ const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-	module: {
-		// 样式文件的处理规则，对css/sass/scss等不同内容使用相应的styleLoaders
-    	// 由utils配置出各种类型的预处理语言所需要使用的loader，例如sass需要使用sass-loader
-		rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
-	},
-	// cheap-module-eval-source-map is faster for development
-	// 使用这种source-map更快
-	devtool: config.dev.devtool,
+    module: {
+        // 样式文件的处理规则，对css/sass/scss等不同内容使用相应的styleLoaders
+        // 由utils配置出各种类型的预处理语言所需要使用的loader，例如sass需要使用sass-loader
+        rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    },
+    // cheap-module-eval-source-map is faster for development
+    // 使用这种source-map更快
+    devtool: config.dev.devtool,
 
-	// these devServer options should be customized in /config/index.js
-	devServer: {
-		clientLogLevel: 'warning',
-		historyApiFallback: {
-			rewrites: [
-				{ from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
-			],
-		},
-		hot: true,
-		contentBase: false, // since we use CopyWebpackPlugin.
-		compress: true,
-		host: HOST || config.dev.host,
-		port: PORT || config.dev.port,
-		open: config.dev.autoOpenBrowser,
-		overlay: config.dev.errorOverlay
-			? { warnings: false, errors: true }
-			: false,
-		publicPath: config.dev.assetsPublicPath,
-		proxy: config.dev.proxyTable,
-		quiet: true, // necessary for FriendlyErrorsPlugin
-		watchOptions: {
-			poll: config.dev.poll,
-		}
-	},
-	// webpack插件
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': require('../config/dev.env')
-		}),
-		// 开启webpack热更新功能
-		new webpack.HotModuleReplacementPlugin(),
-		// 这个插件的主要作用就是在热加载的时候直接返回更新文件的名称，而不是文件的id
-		new webpack.NamedModulesPlugin(),
-		// webpack编译过程中出错的时候跳过报错阶段，不会阻塞编译，在编译结束后报错
-		new webpack.NoEmitOnErrorsPlugin(),
-		// 这个插件主要是生成一个html文件，自动将依赖注入html模板，并输出最终的html文件到目标文件夹
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			//favicon: path.resolve(__dirname, 'favicon.ico'),
-			template: 'index.html',
-			// 将所有的静态文件都插入到body文件的末尾
-			inject: true
-		}),
-		// 拷贝静态资源
-		new CopyWebpackPlugin([
-			{
-				from: path.resolve(__dirname, '../static'),
-				to: config.dev.assetsSubDirectory,
-				ignore: ['.*']
-			}
-		])
-	]
+    // these devServer options should be customized in /config/index.js
+    devServer: {
+        clientLogLevel: 'warning',
+        historyApiFallback: {
+            rewrites: [
+                { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+            ],
+        },
+        hot: true,
+        contentBase: false, // since we use CopyWebpackPlugin.
+        compress: true,
+        host: HOST || config.dev.host,
+        port: PORT || config.dev.port,
+        open: config.dev.autoOpenBrowser,
+        overlay: config.dev.errorOverlay
+            ? { warnings: false, errors: true }
+            : false,
+        publicPath: config.dev.assetsPublicPath,
+        proxy: config.dev.proxyTable,
+        quiet: true, // necessary for FriendlyErrorsPlugin
+        watchOptions: {
+            poll: config.dev.poll,
+        }
+    },
+    // webpack插件
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': require('../config/dev.env')
+        }),
+        // 开启webpack热更新功能
+        new webpack.HotModuleReplacementPlugin(),
+        // 这个插件的主要作用就是在热加载的时候直接返回更新文件的名称，而不是文件的id
+        new webpack.NamedModulesPlugin(),
+        // webpack编译过程中出错的时候跳过报错阶段，不会阻塞编译，在编译结束后报错
+        new webpack.NoEmitOnErrorsPlugin(),
+        // 这个插件主要是生成一个html文件，自动将依赖注入html模板，并输出最终的html文件到目标文件夹
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            //favicon: path.resolve(__dirname, 'favicon.ico'),
+            template: 'index.html',
+            // 将所有的静态文件都插入到body文件的末尾
+            inject: true
+        }),
+        // 拷贝静态资源
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, '../static'),
+                to: config.dev.assetsSubDirectory,
+                ignore: ['.*']
+            }
+        ])
+    ]
 })
 
 module.exports = new Promise((resolve, reject) => {
-	portfinder.basePort = process.env.PORT || config.dev.port
-	// 这种获取port的方式会返回一个promise
-	portfinder.getPort((err, port) => {
-		if (err) {
-			reject(err)
-		} else {
-			// 把获取到的端口号设置为环境变量PORT的值
-			process.env.PORT = port
-			// 重新设置webpack-dev-server的端口的值
-			devWebpackConfig.devServer.port = port
+    portfinder.basePort = process.env.PORT || config.dev.port
+    // 这种获取port的方式会返回一个promise
+    portfinder.getPort((err, port) => {
+        if (err) {
+            reject(err)
+        } else {
+            // 把获取到的端口号设置为环境变量PORT的值
+            process.env.PORT = port
+            // 重新设置webpack-dev-server的端口的值
+            devWebpackConfig.devServer.port = port
 
-			// 将FriendlyErrorsPlugin添加到webpack的配置文件中
-			devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-				// 编译成功时候的输出信息
-				compilationSuccessInfo: {
-					messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
-				},
-				// 当编译出错的时候，根据config.dev.notifyOnErrors来确定是否需要在桌面右上角显示错误通知框
-				onErrors: config.dev.notifyOnErrors
-				? utils.createNotifierCallback()
-				: undefined
-			}))
-			// resolve我们的配置文件
-			resolve(devWebpackConfig)
-		}
-	})
+            // 将FriendlyErrorsPlugin添加到webpack的配置文件中
+            devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+                // 编译成功时候的输出信息
+                compilationSuccessInfo: {
+                    messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+                },
+                // 当编译出错的时候，根据config.dev.notifyOnErrors来确定是否需要在桌面右上角显示错误通知框
+                onErrors: config.dev.notifyOnErrors
+                ? utils.createNotifierCallback()
+                : undefined
+            }))
+            // resolve我们的配置文件
+            resolve(devWebpackConfig)
+        }
+    })
 })
--- template/src/api/api.js
+++ template/src/api/api.js
@@ -4,95 +4,89 @@ import store from '../store'
 import Common from '@/utils/common'
 import { USER_TOKEN } from '@/maps/constants'
 const instance = axios.create({
-	timeout: 15000
+    timeout: 15000
 })
 // request拦截器
 instance.interceptors.request.use(config => {
-	if (store.getters.token) {
-		// 让每个请求携带自定义token 请根据实际情况自行修改
-		config.headers['X-Token'] = Common.getCookie(USER_TOKEN)
-	}
-	return config
+    if (store.getters.token) {
+        // 让每个请求携带自定义token 请根据实际情况自行修改
+        config.headers['X-Token'] = Common.getCookie(USER_TOKEN)
+    }
+    return config
 }, error => {
-	console.log(error)
-	Promise.reject(error)
+    Promise.reject(error)
 })
 
 // respone拦截器
 instance.interceptors.response.use(
-	response => {
-		// returncode为非10000时抛错,可结合自己业务进行修改
-		const res = response.data
-		if (res.returncode !== '10000') {
-			// 50008:非法的token
-			if (res.returncode === '50008') {
-				MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-					confirmButtonText: '重新登录',
-					cancelButtonText: '取消',
-					type: 'warning'
-				}).then(() => {
-					store.dispatch('User/fedLogOut').then(() => {
-						// 为了重新实例化vue-router对象 避免bug
-						location.reload()
-					})
-				})
-			} else {
-				Message({
-					message: res.data,
-					type: 'error',
-					duration: 3 * 1000
-				})
-			}
-			return Promise.reject(new Error('error'))
-		} else {
-			return response
-		}
-	},
-	error => {
-		console.log('err' + error)
-		Message({
-			message: error.message,
-			type: 'error',
-			duration: 3 * 1000
-		})
-		return Promise.reject(error)
-	}
+    response => {
+        // code为非10000时抛错,可结合自己业务进行修改
+        const res = response.data
+        if (res.code !== 10000) {
+            // 50008:非法的token
+            if (res.code === 50008) {
+                MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
+                    confirmButtonText: '重新登录',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    store.dispatch('User/fedLogOut').then(() => {
+                        // 为了重新实例化vue-router对象 避免bug
+                        location.reload()
+                    })
+                })
+            } else {
+                Message({
+                    message: res.data,
+                    type: 'error',
+                    duration: 3 * 1000
+                })
+            }
+            return Promise.reject(new Error('error'))
+        } else {
+            return response
+        }
+    },
+    error => {
+        Message({
+            message: error.message,
+            type: 'error',
+            duration: 3 * 1000
+        })
+        return Promise.reject(error)
+    }
 )
 const API = {
-	get (url, params, callback) {
-		return new Promise((resolve, reject) => {
-			instance.get(url, {
-				params: params
-			}).then((response) => {
-				if (response.data.returncode === '10000') {
-					callback && callback(response.data.body)
-					resolve(response.data.body)
-				} else {
-					console.log('服务器错误:' + response.data.message)
-					reject(response.data.message)
-				}
-			}).catch((error) => {
-				console.log('服务器错误!' + error)
-				reject(error)
-			})
-		})
-	},
-	post (url, params, callback) {
-		return new Promise((resolve, reject) => {
-			instance.post(url, params).then((response) => {
-				if (response.data.returncode === '10000') {
-					callback && callback(response.data.body)
-					resolve(response.data.body)
-				} else {
-					console.log('服务器错误:' + response.data.message)
-					reject(response.data.message)
-				}
-			}).catch((error) => {
-				console.log('服务器错误!' + error)
-				reject(error)
-			})
-		})
-	}
+    get (url, params, callback) {
+        return new Promise((resolve, reject) => {
+            instance.get(url, {
+                params: params
+            }).then((response) => {
+                if (response.data.code === 10000) {
+                    callback && callback(response.data.body)
+                    resolve(response.data.body)
+                } else {
+                    reject(response.data.message)
+                }
+            }).catch((error) => {
+                reject(error)
+            })
+        })
+    },
+    post (url, params, callback) {
+        return new Promise((resolve, reject) => {
+            instance.post(url, params).then((response) => {
+                if (response.data.code === 10000) {
+                    callback && callback(response.data.body)
+                    resolve(response.data.body)
+                } else {
+                    reject(response.data.message)
+                }
+            }).catch((error) => {
+                reject(error)
+            })
+        })
+    }
 }
 
 export { API }
